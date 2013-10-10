a1b86d6f07a37e24b47a717952b5c5e8c8cbb1ef
richar <ricardosiri68@gmail.com>
2013-10-10 16:53:06 -0300

    quitandole is_home deberia marchar

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,6 @@
 <div id="sidebar">
-	<?php if (is_home()) { ?>
-		<div id="secondcol">
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?>
-			<?php endif; ?>
-		</div> <!-- end #secondcol -->
-	<?php }; ?>
-
+	<div id="secondcol">
+		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?>
+		<?php endif; ?>
+	</div> <!-- end #secondcol -->
 </div> <!-- end sidebar -->
